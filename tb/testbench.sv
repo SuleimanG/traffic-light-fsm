d76c6faaae5b8d57606ae200da2ef04f140982ca
SuleimanG <mohamedsoliman9091@gmail.com>
2026-09-18 12:39:27 +0300

    Add done signal and modify check_transition task
    Added 'done' signal to indicate when the counter is finished and updated check_transition task to wait for this signal before proceeding.

--- tb/testbench.sv
+++ tb/testbench.sv
@@ -3,6 +3,7 @@ module tb_top_module ();
     // signal declaration
     logic T_A, T_B, clk, rst;
     logic [1:0] L_A, L_B;
+    logic done; // signal to indicate when the counter is done
 
     int errors = 0;  //  error counter   
 
@@ -13,7 +14,8 @@ module tb_top_module ();
         .clk(clk),
         .RESET(rst),
         .L_A(L_A),
-        .L_B(L_B)
+        .L_B(L_B),
+        .done(done)
     );
 
     // clock generation
@@ -25,22 +27,28 @@ module tb_top_module ();
 
     task check_transition(
         input logic ta, tb,
-        input logic [1:0] exp_LA, exp_LB
+        input logic [1:0] exp_LA, exp_LB,
+        input logic wait_for_done // whether to wait for the counter to finish
     );
 
         T_A = ta;
         T_B = tb;
 
         @(posedge clk); //  wait one cycle
+        if (wait_for_done)
+            begin
+                wait(done); // wait for the counter to finish
+              	@(posedge clk); // wait for the next clock edge after done is asserted
+            end
         #1; //  delta waiting time
         if (L_A !== exp_LA || L_B !== exp_LB)   //  check for expected output
             begin
-            $display("FAILED AT T_A=%b, T_B=%b: Expected L_A=%b, L_B=%b but got L_A=%b, L_B=%b", ta, tb, exp_LA, exp_LB, L_A, L_B);
-            errors++;
+                $display("FAILED AT T_A=%b, T_B=%b: Expected L_A=%b, L_B=%b but got L_A=%b, L_B=%b", ta, tb, exp_LA, exp_LB, L_A, L_B);
+                errors++;
             end
         else
             begin
-            $display("PASSED AT T_A=%b, T_B=%b: L_A=%b, L_B=%b", ta, tb, L_A, L_B);
+                $display("PASSED AT T_A=%b, T_B=%b: L_A=%b, L_B=%b", ta, tb, L_A, L_B);
             end
     endtask
 
@@ -61,42 +69,48 @@ module tb_top_module ();
             check_transition(
                 .ta(1'b1),
                 .tb(1'b0),    // don't care, can be assigned to either 0 or 1
-                .exp_LA(2'b00), .exp_LB(2'b10) // 00 green, 01 yellow, 10 red
+                .exp_LA(2'b00), .exp_LB(2'b10), // 00 green, 01 yellow, 10 red
+                .wait_for_done(1'b0)
             );
 
             // test case 2: S0 --> S1
             check_transition(
                 .ta(1'b0),
                 .tb(1'b0),    // don't care, can be assigned to either 0 or 1
-                .exp_LA(2'b01), .exp_LB(2'b10) // 00 green, 01 yellow, 10 red
+                .exp_LA(2'b01), .exp_LB(2'b10), // 00 green, 01 yellow, 10 red
+                .wait_for_done(1'b0)    
             );
 
             // test case 3: S1 --> S2
             check_transition(
                 .ta(1'b1),    // don't care, can be assigned to either 0 or 1
                 .tb(1'b0),    // don't care, can be assigned to either 0 or 1
-                .exp_LA(2'b10), .exp_LB(2'b00) // 00 green, 01 yellow, 10 red
+                .exp_LA(2'b10), .exp_LB(2'b00), // 00 green, 01 yellow, 10 red
+                .wait_for_done(1'b1) // wait for the counter to finish
             );
 
             // test case 4: S2 --> S2
             check_transition(
                 .ta(1'b1),    // don't care, can be assigned to either 0 or 1
                 .tb(1'b1),    
-                .exp_LA(2'b10), .exp_LB(2'b00) // 00 green, 01 yellow, 10 red
+                .exp_LA(2'b10), .exp_LB(2'b00), // 00 green, 01 yellow, 10 red
+                .wait_for_done(1'b0)
             );
 
             // test case 5: S2 --> S3
             check_transition(
                 .ta(1'b0),    // don't care, can be assigned to either 0 or 1
                 .tb(1'b0),    
-                .exp_LA(2'b10), .exp_LB(2'b01) // 00 green, 01 yellow, 10 red
+                .exp_LA(2'b10), .exp_LB(2'b01), // 00 green, 01 yellow, 10 red
+                .wait_for_done(1'b0)
             );
 
             // test case 6: S3 --> S0
             check_transition(
                 .ta(1'b1),    // don't care, can be assigned to either 0 or 1
                 .tb(1'b0),    // don't care, can be assigned to either 0 or 1
-                .exp_LA(2'b00), .exp_LB(2'b10) // 00 green, 01 yellow, 10 red
+                .exp_LA(2'b00), .exp_LB(2'b10), // 00 green, 01 yellow, 10 red
+                .wait_for_done(1'b1) // wait for the counter to finish
             );
 
             // hold final state briefly so the waveform viewer can render it
